83c3a32bbc774faca796bc3c79001571db238c7f
Łukasz Starosta <lstarosta98@gmail.com>
2020-05-13 10:19:44 +0200

    Initial commit with two module setup

---
+++ musiceventsapi/src/main/java/pl/dmcs/lstarosta/musiceventsapi/MusiceventsapiApplication.java
@@ -0,0 +1,13 @@
+package pl.dmcs.lstarosta.musiceventsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiceventsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiceventsapiApplication.class, args);
+	}
+
+}
