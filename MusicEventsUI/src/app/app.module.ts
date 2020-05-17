8554b34621f6c78ff7f7c412df5d4f89b9631a40
Łukasz Starosta <lstarosta98@gmail.com>
2020-05-17 09:45:11 +0200

    Added a few basic routes

--- MusicEventsUI/src/app/app.module.ts
+++ MusicEventsUI/src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {LandingComponent} from './landing/landing.component';
+import { EventsComponent } from './events/events.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'events', component: EventsComponent},
+  {path: 'profile', component: ProfileComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    EventsComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
