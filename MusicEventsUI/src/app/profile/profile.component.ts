bfc69a0a1deeb7c9877a1cf15aeaf72d61dafdf3
Łukasz Starosta <lstarosta98@gmail.com>
2020-05-30 11:10:04 +0200

    Added a simple edit form for profile

--- MusicEventsUI/src/app/profile/profile.component.ts
+++ MusicEventsUI/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ISignUp} from "../types/ISignUp";
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  form: ISignUp = {email: '', password: '', firstName: '', lastName: ''};
+  signupInfo: ISignUp;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.signupInfo = {
+      email: this.form.email,
+      password: this.form.password,
+      firstName: this.form.firstName,
+      lastName: this.form.lastName
+    }
+
+    console.log(this.signupInfo);
+  }
 }
